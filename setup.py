352349eb6b5d40fa4bc89b6719adc063b245332c
rizac <riccardo.zaccarelli@gfz-potsdam.de>
2020-06-18 15:59:29 +0200

    adds model and setup

---
+++ setup.py
@@ -0,0 +1,57 @@
+from setuptools import setup, find_packages
+
+_README = """
+Python program/library to compute amplitude anomaly score on one or more seismic
+waveforms (data and metadata)
+"""
+
+setup(
+    name='swaas',
+    version='1.1.0',
+    description=_README,
+    url='https://github.com/rizac/swaas',
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    python_requires='>=3.7.3',
+    # Minimal requirements, for a complete list see requirements-*.txt
+    install_requires=[
+        'numpy>=1.15.4',
+        'obspy>=1.1.1',
+        'scikit-learn>=0.21.3'
+    ],
+    # List additional groups of dependencies here (e.g. development
+    # dependencies). You can install these using the following syntax,
+    # for example:
+    # $ pip install -e .[jupyter,test]
+    extras_require={
+        'jupyter': [
+            'jupyter>=1.1.0'
+        ],
+        'test': []
+    },
+    author='riccardo zaccarelli',
+    author_email='',  # FIXME: what to provide?
+    maintainer='Section 2.6 (Seismic Hazard and Risk Dynamics), GFZ Potsdam',  # FIXME
+    maintainer_email='',
+    classifiers=(
+        'Development Status :: 1 - Beta',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU Affero General Public License v3',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering',
+    ),
+    keywords=[
+        "seismic waveform",
+        "isolation forest",
+        "outlier score",
+        "anomaly detection",
+        "machine learning"
+    ],
+    license="GPL3",
+    platforms=["any"],  # FIXME: shouldn't be unix/macos? (shallow google search didn't help)
+    # package_data={"smtk": [
+    #    "README.md", "LICENSE"]},
+    # include_package_data=True,
+    zip_safe=False,
+)
